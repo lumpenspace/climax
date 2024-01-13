60cb96db3ab33d6c26bd2ae4735f9f9ec5daae61
lumpenspace <125578322+lumpenspace@users.noreply.github.com>
2024-01-13 06:07:13 +0100

    Fix tests

--- clipboard_maximizer/clipboard_manager.py
+++ clipboard_maximizer/clipboard_manager.py
@@ -7,6 +7,8 @@ def copy_to_clipboard(files, climax_clipboard=None):
     system_platform = platform.system()
     climax_clipboard = climax_clipboard or os.getenv('CLIMAX_CLIPBOARD')
 
+    abs_path = os.path.abspath(__file__)
+    dir_name = os.path.dirname(abs_path)
     if system_platform == 'Darwin' or climax_clipboard == 'applescript':
         try:
             subprocess.run(['which', 'osascript'], check=True)
@@ -14,7 +16,7 @@ def copy_to_clipboard(files, climax_clipboard=None):
             print("osascript is not installed. Please install it to use this feature.")
             sys.exit(1)
         try:
-            script_path = './clipboard_maximizer/copy-macos.applescript'
+            script_path = os.path.join(dir_name, 'copy-macos.applescript')
             subprocess.run(['osascript', script_path] + list(files), check=True)
         except subprocess.CalledProcessError as e:
             print("Failed to copy to the MacOS clipboard.", e.output)
@@ -22,13 +24,13 @@ def copy_to_clipboard(files, climax_clipboard=None):
     elif (system_platform == 'Linux' or climax_clipboard == 'xclip'):
         try:
             subprocess.run(['which', 'xclip'], check=True)
-            script_path = 'clipboard_maximizer/copy-linux-xclip.sh'
+            script_path = os.path.join(dir_name, 'copy-linux-xclip.sh')
             subprocess.run([script_path] + list(files), check=True)
         except subprocess.CalledProcessError:
             raise Exception("install xclip to copy the file to clipboard")
     elif (system_platform == 'Windows' or climax_clipboard == 'powershell'):
         try:
-            script_path = 'clipboard_maximizer/copy-windows.ps1'
+            script_path = os.path.join(dir_name, 'copy-windows.ps1')
             # PowerShell execution policy might prevent the script from running, hence the bypass
             subprocess.run(['powershell', '-ExecutionPolicy', 'Bypass', '-File', script_path] + list(files), check=True)
         except subprocess.CalledProcessError as e:
--- tests/test_clipboard_manager.py
+++ tests/test_clipboard_manager.py
@@ -4,29 +4,36 @@ from unittest.mock import patch, MagicMock
 from clipboard_maximizer.clipboard_manager import copy_to_clipboard
 import platform
 import subprocess
+import os
+
+scripts_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'clipboard_maximizer'))
 
 class TestClipboardManager(unittest.TestCase):
-
     @patch('platform.system')
     def test_copy_to_clipboard_macos(self, mock_platform):
         mock_platform.return_value = 'Darwin'
         with patch('subprocess.run') as mock_run:
             copy_to_clipboard(['testfile.txt'])
-            mock_run.assert_called_with(['osascript', 'zippity/copy-macos.applescript', 'testfile.txt'], check=True)
+            mock_run.assert_called_with(['osascript', os.path.join(scripts_dir, 'copy-macos.applescript'), 'testfile.txt'], check=True)
 
     @patch('platform.system')
     def test_copy_to_clipboard_linux(self, mock_platform):
         mock_platform.return_value = 'Linux'
         with patch('subprocess.run') as mock_run:
             copy_to_clipboard(['testfile.txt'])
-            mock_run.assert_called_with(['zippity/copy-linux-xclip.sh', 'testfile.txt'], check=True)
+            mock_run.assert_called_with([os.path.join(scripts_dir, 'copy-linux-xclip.sh'), 'testfile.txt'], check=True)
 
     @patch('platform.system')
     def test_copy_to_clipboard_windows(self, mock_platform):
         mock_platform.return_value = 'Windows'
         with patch('subprocess.run') as mock_run:
             copy_to_clipboard(['testfile.txt'])
-            mock_run.assert_called_with(['powershell', '-ExecutionPolicy', 'Bypass', '-File', 'clipboard_maximizer/copy-windows.ps1', 'testfile.txt'], check=True)
+            mock_run.assert_called_with([
+                'powershell',
+                '-ExecutionPolicy', 'Bypass', 
+                '-File', os.path.join(scripts_dir, 'copy-windows.ps1'),
+                'testfile.txt'
+            ], check=True)
 
     @patch('platform.system')
     def test_copy_to_clipboard_unsupported_os(self, mock_platform):
